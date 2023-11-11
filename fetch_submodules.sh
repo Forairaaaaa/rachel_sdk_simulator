9b4c15bb0dc848267c25fc9731f73e27f5f3233c
Forairaaaaa <applesyqd@outlook.com>
2023-11-11 09:52:40 +0800

    change fetch script

--- fetch_submodules.sh
+++ fetch_submodules.sh
@@ -1,6 +1,10 @@
-git submodule init
-git submodule update
-cd Lib/LovyanGFX
-git checkout master
-cd ../mooncake
-git checkout main
+# git submodule init
+# git submodule update
+# cd Lib/LovyanGFX
+# git checkout master
+# cd ../mooncake
+# git checkout main
+
+cd Lib
+git clone git@github.com:Forairaaaaa/LovyanGFX.git
+git clone https://github.com/Forairaaaaa/mooncake.git
