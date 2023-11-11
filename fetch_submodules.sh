814e8e42e8c27022c1bd8584d0833c79cd4a8e58
Forairaaaaa <applesyqd@outlook.com>
2023-11-11 20:13:38 +0800

    change back

--- fetch_submodules.sh
+++ fetch_submodules.sh
@@ -1,10 +1,6 @@
-# git submodule init
-# git submodule update
-# cd Lib/LovyanGFX
-# git checkout master
-# cd ../mooncake
-# git checkout main
-
-cd Lib
-git clone git@github.com:Forairaaaaa/LovyanGFX.git
-git clone https://github.com/Forairaaaaa/mooncake.git
+git submodule init
+git submodule update
+cd Lib/LovyanGFX
+git checkout master
+cd ../mooncake
+git checkout main
