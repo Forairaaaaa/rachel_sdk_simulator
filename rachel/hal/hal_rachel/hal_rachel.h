2c9c599a4072be816073134174c7c026cbbc84c8
Forairaaaaa <applesyqd@outlook.com>
2023-11-17 09:06:16 +0800

    sync to rachel sdk

--- rachel/hal/hal_rachel/hal_rachel.h
+++ rachel/hal/hal_rachel/hal_rachel.h
@@ -13,17 +13,18 @@
 #include "utils/m5unified/I2C_Class.hpp"
 #include "utils/m5unified/RTC8563_Class.hpp"
 #include "utils/m5unified/IMU_Class.hpp"
-#include "utils/m5unified/Speaker_Class.hpp"
+// #include "utils/m5unified/Speaker_Class.hpp"
 
 
 class HAL_Rachel : public HAL
 {
 private:
     std::array<uint8_t, 11> _gamepad_key_map;
+    std::array<bool, 11> _key_state_list;
     m5::I2C_Class* _i2c_bus;
     m5::RTC8563_Class* _rtc;
     m5::IMU_Class* _imu;
-    m5::Speaker_Class* _spk;
+    // m5::Speaker_Class* _spk;
 
 private:
     void _power_init();
@@ -37,21 +38,22 @@ private:
     void _sdcard_init();
     void _gamepad_init();
     void _adjust_sys_time();
+    void _system_config_init();
     void _sum_up();
 
 public:
     HAL_Rachel() :
         _i2c_bus(nullptr),
         _rtc(nullptr),
-        _imu(nullptr),
-        _spk(nullptr)
+        _imu(nullptr)
+        // _spk(nullptr)
         {}
     ~HAL_Rachel()
     {
         delete _rtc;
         delete _imu;
         delete _i2c_bus;
-        delete _spk;
+        // delete _spk;
     }
 
     inline std::string type() override { return "Rachel"; }
@@ -67,6 +69,7 @@ public:
         _imu_init();
         _fs_init();
         _sdcard_init();
+        _system_config_init();
         _sum_up();
     }
 
@@ -81,4 +84,6 @@ public:
     void updateImuData() override;
     void beep(float frequency, uint32_t duration) override;
     void setBeepVolume(uint8_t volume) override;
+    void loadSystemConfig() override;
+    void saveSystemConfig() override;
 };
